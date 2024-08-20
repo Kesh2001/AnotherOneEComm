6cc0429c89801e7591f5ba9550c0a6f5bc93aa65
Keshav Anand <keshavanand2001@gmail.com>
2024-08-20 03:22:15 -0400

    checkout page added

---
+++ src/app/checkout/page.js
@@ -0,0 +1,8 @@
+import Layout from "../../components/Layout"
+export default function CheckoutPage(){
+    return(
+        <Layout>
+            Checkout
+        </Layout>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,26 @@
 import mongoose from '../../lib/mongoose';
 import ProductModel from '../../models/product';
 import ClientSideProducts from '../components/ClientSideProducts';
+import Footer from '../components/Footer';
+import Layout from '../components/Layout';
 
 export default async function Home() {
   await mongoose.connect(process.env.MONGODB_URL);
   const products = await ProductModel.find().lean();
+  const plainProducts = products.map(product => ({
+    ...product,
+    _id: product._id.toString(),
+    description: product.description.toString(),
+    price: product.price.toString(),
+    catrgory: product.category.toString(),
+    picture: product.picture.toString(),
+    name: product.name.toString(),
 
+  }));
   return (
-    <div className="p-5">
-      <ClientSideProducts products={products} />
-    </div>
+    
+    <Layout>
+      <ClientSideProducts products={plainProducts} />
+    </Layout>
   );
 }
