40c669855a17d1793d23432bf1e9d24e6140c218
Keshav Anand <keshavanand2001@gmail.com>
2024-08-21 16:43:18 -0400

    Checkout page objs coming

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,8 +1,25 @@
+'use client';
+
+import { useContext, useEffect, useState } from "react"
 import Layout from "../../components/Layout"
+import { ProductsContext } from "../../components/ProductsContext"
+
+
 export default function CheckoutPage(){
+
+    const {selectedProducts} = useContext(ProductsContext)
+    const [ProductsInfo, setProductsInfo]=useState([])
+    
+    useEffect(()=>{
+        const uniqIds = [...new Set(selectedProducts)]
+
+        fetch('/api/products?ids='+uniqIds.join(','))
+        .then(response=>response.json()).then(json=>setProductsInfo(json))
+    },[selectedProducts])
+
     return(
         <Layout>
-            Checkout
+            {selectedProducts.join(',')}
         </Layout>
     )
 }
--- src/components/ProductsContext.js
+++ src/components/ProductsContext.js
@@ -1,13 +1,22 @@
 'use client';
 
-import { createContext, useState } from 'react';
+import { createContext, useEffect } from 'react';
+import useLocalStorageState from 'use-local-storage-state';
 
 // Create the context
 export const ProductsContext = createContext();
 
 // Create the provider component
 export function ProductsProvider({ children }) {
-    const [selectedProducts, setSelectedProducts] = useState([]);
+    // Automatically handles local storage, storing `selectedProducts` under the key 'cart'
+    const [selectedProducts, setSelectedProducts] = useLocalStorageState('cart', {
+        defaultValue: 2 // Initializes with an empty array if nothing is in local storage
+    });
+
+    // For debugging purposes, we log the cart contents whenever they change
+    useEffect(() => {
+        console.log('Cart:', selectedProducts);
+    }, [selectedProducts]);
 
     return (
         <ProductsContext.Provider value={{ selectedProducts, setSelectedProducts }}>
